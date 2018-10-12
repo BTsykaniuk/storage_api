d48dcf715d076c2d237448bce957fbc96aee4673
BTsykaniuk <btsykanjuk@i.ua>
2018-10-12 22:37:26 +0300

    Add API points

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,32 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from products.models import Product
 
 
-class ProductSerializer(ModelSerializer):
+class ProductListSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        products = [Product(**item) for item in validated_data]
+        return Product.objects.bulk_create(products)
+
+    def update(self, instance, validated_data):
+        product_map = {product.id: product for product in instance}
+        data_map = {item['id']: item for item in validated_data}
+
+        out = []
+        for product_id, data in data_map.items():
+            product = product_map.get(product_id)
+            out.append(self.child.update(product, data))
+        return out
+
+    def delete(self, instance):
+        for item in instance:
+            item.delete()
+        return instance
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('id', 'name', 'description', 'date_added', 'date_updated')
+        list_serializer_class = ProductListSerializer
--- api/views.py
+++ api/views.py
@@ -1,8 +1,47 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+
 from products.models import Product
 from api.serializers import ProductSerializer
+
+
+class ListMixin:
+    def get_serializer(self, *args, **kwargs):
+        if "data" in kwargs:
+            data = kwargs["data"]
+
+            if isinstance(data, list):
+                kwargs["many"] = True
+
+        return super().get_serializer(*args, **kwargs)
+
+
+class ProductObjectMixin:
+    def get_object(self):
+        ids_list = self.get_update_ids(self.request.data)
+        return Product.objects.filter(id__in=ids_list)
+
+    @staticmethod
+    def get_update_ids(data):
+        ids = []
+        for item in data:
+            ids.append(item['id'])
+        return ids
 
 
 class ProductListAPIView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductsCreateAPIView(ListMixin, CreateAPIView):
+    serializer_class = ProductSerializer
+
+
+class ProductUpdateAPIView(ListMixin, ProductObjectMixin, UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDeleteAPIView(ListMixin, ProductObjectMixin, DestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
